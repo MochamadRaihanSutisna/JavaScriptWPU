32bd13e41542046f66b34e1ce3c9f0c6676da508
unknown <mochamadraihansutisna@gmail.com>
2023-11-17 21:58:50 +0700

    revisi

--- 3.1 latihanScope.js
+++ 3.1 latihanScope.js
@@ -1,3 +1,4 @@
+/*
 console.log(sayHello());
 
 var nama = "Raihan";
@@ -5,4 +6,22 @@ var umur = 33;
 
 function sayHello() {
     return `Halo, nama saya ${nama}, umur saya ${umur} tahun`;
-}
+}*/
+
+// function a() {
+//     console.log("ini a");
+    
+//     function b() {
+//         console.log("ini b");
+        
+//         function c() {
+//             console.log("ini c");
+//         }
+        
+//         c();
+//     }
+    
+//     b();
+// }
+
+// a();
